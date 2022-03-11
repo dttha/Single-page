ff60703e1791fe0b05d7ee663c019c130fbaddfb
đttha <dothithuha140@gmail.com>
2022-03-11 10:15:59 +0700

    add banner

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import line from '../../../public/line 4_1.png'
 import styles from '../Content/content.module.css'
 
 export default function Content() {
     return (
         <div className={styles.content}>
-
+            <div className={styles.wrap_content}>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className={styles.context}>
+                            <span className={styles.fontWhite}>Create</span>
+                            <span className={styles.fontGreen}> Branded</span>
+                            <span className={styles.fontWhite}> eCommerce Experience that</span>
+                            <span className={styles.fontGreen}> converts!</span>
+                        </div>
+                        <div className={styles.description}>
+                            We champion meaningful brands, products and services by creating an intuitive and codeless eCommerce experience that helps you to engagingly communicate your brand and build a strong connection with your audience.
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <div className={styles.select_btn}>Explore</div>&nbsp;&nbsp;
+                        <FontAwesomeIcon className={styles.font_icon} icon={faCaretDown} />
+                    </div>
+                </div>
+            </div>
+            <Image alt='line' className={styles.line} src={line} width="2327" height="725.05" />
         </div>
     )
 }
